7065c2ceb91ee251e4b14ff361d6f9c37e2a9e4b
magoafono <simone.marchi@ilc.cnr.it>
2026-09-04 13:31:47 +0200

    fatto scorrere il contesto sinistro mantenendo visibile la parte vicina alla keyword, versione 0.14.14

--- app/page.tsx
+++ app/page.tsx
@@ -205,7 +205,7 @@ function getServerLangSnapshot(): Lang {
   return "it";
 }
 
-const appVersion = "0.14.13";
+const appVersion = "0.14.14";
 
 const basePath = (process.env.NEXT_PUBLIC_BASE_PATH ?? "/futuri-impossibili").replace(/\/$/, "");
 
